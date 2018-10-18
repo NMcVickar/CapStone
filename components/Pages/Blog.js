5868a2cb16423044545d833519ecf25e3484dee4
Nick McVickar <NMcVickar@aol.com>
2018-10-18 17:49:37 -0500

    Depression Facts

--- components/Pages/Blog.js
+++ components/Pages/Blog.js
@@ -7,10 +7,10 @@ function Repo(repo){
         .join(' ');
 
     return `
-    <li>
+<li>
     <a href="${repo.url}"> ${name}</a>
-    </li>
-    `;
+</li>
+`;
 }
 export default function Blog(state){
     var repos = state.repos.map(Repo).join('');
@@ -18,47 +18,88 @@ export default function Blog(state){
     return `
 <div id=content>
     <div class="container">
-        <p>Here is a Little Bit About Me and My Background.</p>
-        <div span class="about">
-            <ul>
-                <li>
-                    What I Know
-                    <ul class ="dropdown">
-                        <li>Robotics C & C++</li>
-                        <li>Java</li>
-                        <li>JavaScript</li>
-                        <li>CSS3</li>
-                        <li>HTML5</li>
-                        <li>JSON</li>
-                        <li>Web API</li>
-                    </ul>
-                </li>
+        <div class="why">
+            <h3>Why Did I Decide to Make This Website</h3>
+            <p>
+                For the past couple of months i have been dealing with Depression and Anxiety along with other issues.
+                Many of the websites I looked at that talk about these issues do so in a way that is either not
+                straight-forward or confusing.
+                I wanted to display my understanding of web development while also making an attempt to help people if
+                i can.
+                This website was not the first iteration of my project, but after several false starts I decided that
+                this would be a way that I could display my knowledge in the little time I had left.
+            </p>
 
-                <li>
-                    Experience
-                    <ul class = "dropdown">
-                        <li>Website Maintence</li>
-                        <li>WordPress Management</li>
-                        <li>Data Entry</li>
-                        <li>Inventory Management</li>
+            <br>
+
+            <p>
+                I hope that I was able to make sense of the information that i have compiled here in a way that is
+                easily understandable.
+                Here is a list of sources that I gathered my information from:
+                <ul>
+                    <li>
+                        <a href="https://adaa.org/understanding-anxiety/depression">
+                            https://adaa.org/understanding-anxiety/depression
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.nimh.nih.gov/health/statistics/major-depression.shtml">
+                            https://www.nimh.nih.gov/health/statistics/major-depression.shtml
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.mentalhealth.gov/what-to-look-for/mood-disorders/depression">
+                            https://www.mentalhealth.gov/what-to-look-for/mood-disorders/depression
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<li>
+                        <a href="https://www.nimh.nih.gov/health/topics/anxiety-disorders/index.shtml">
+                        https://www.nimh.nih.gov/health/topics/anxiety-disorders/index.shtml
+                        </a>
+                    </li>
                     </ul>
-                </li>
-                
-                <li>
-                    Personal Projcts
-                    <ul class = "dropdown">
-                        <li>Server Maintence</li>
-                        <li>Networking</li>
-                        <li>System Assembly</li>
-                    </ul>
-                </li>
-            </ul>
-		</div>
-		<p>Here are links to my Github Repositories.</p>
-        <ol>
-        ${repos}
-        </ol>
-	</div>
-</div>
-`;
+                    </p> </div> <p>Here is a Little Bit About Me and My Background:</p>
+            <div span class="about">
+                <ul>
+                    <li>
+                        What I Know
+                        <ul class="dropdown">
+                            <li>Robotics C & C++</li>
+                            <li>Java</li>
+                            <li>JavaScript</li>
+                            <li>CSS3</li>
+                            <li>HTML5</li>
+                            <li>JSON</li>
+                            <li>Web API</li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        Experience
+                        <ul class="dropdown">
+                            <li>Website Maintence</li>
+                            <li>WordPress Management</li>
+                            <li>Data Entry</li>
+                            <li>Inventory Management</li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        Personal Projcts
+                        <ul class="dropdown">
+                            <li>Server Maintence</li>
+                            <li>Networking</li>
+                            <li>System Assembly</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <p>Here are links to my Github Repositories.</p>
+            <ol>
+                ${repos}
+            </ol>
+        </div>
+    </div>
+    `;
 }
--- components/Pages/Home.js
+++ components/Pages/Home.js
@@ -1,12 +1,13 @@
 export default function(state){
     return `
-    <h1>Welcome to my Website.</h1>
-    <p>I have made this site to assist people with mental health issues, specificaly depression and anxiety.</p>
-    <br>
-    <h3>What is Depression?</h3>
-<p>Depression is an issue involving your brain. It may manifest as a feeling of being "down" or "blue" that sticks around for days. 
-It will eventually start to interfere in you're day to day life. Symptoms may include:</p>
-    <ul>
+<h1>Welcome to my Website.</h1>
+<p>I have made this site to assist people with mental health issues, specificaly depression and anxiety.</p>
+<br>
+<h3>What is Depression?</h3>
+<p>Depression is an issue involving your brain. It may manifest as a feeling of being "down" or "under the weather"
+    that sticks around for days.
+    It will eventually start to interfere in you're day to day life. Symptoms may include:</p>
+<ul>
     <li>Sadness</li>
     <li>Loss of interest or pleasure in activities you used to enjoy</li>
     <li>Change in weight</li>
@@ -14,6 +15,27 @@ It will eventually start to interfere in you're day to day life. Symptoms may in
     <li>Energy loss</li>
     <li>Feelings of worthlessness</li>
     <li>Thoughts of death or suicide</li>
-   </ul>
+</ul>
+<sub>
+    List Courtsey of
+    <a href="https://www.mentalhealth.gov/what-to-look-for/mood-disorders/depression" target="_blank">
+        MentalHealth.gov
+    </a>.
+</sub>
+<br>
+<br>
+<h3>What is Anxiety?</h3>
+<p>
+    Anxiety is something that everyone will have to deal with from time to time in their lives. 
+    People with an anxiety disorder the feeling of worry or fear stays around and possibly gets worse over time.
+    There are several types of anxiety disorders, including generalized anxiety disorder, panic disorder, and various phobia-related disorders.
+</p>
+<br>
+<p>
+    I have delt with anxiety in my life and want to make this information more available to people. 
+    My sister has experienced traumatic events and we have had to help her with her anxiety.
+    I have experience with trying to deal with it from an exteranl perspective and want people to know that it can be difficult, but it is worth it to get through it.
+    
+</p>
 `;
 }
